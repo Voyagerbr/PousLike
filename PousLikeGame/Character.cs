2d5995b5443f063ab690167ea10c91532965b76b
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-03-28 11:55:11 -0300

    COmmmit day 28/03

---
+++ PousLikeGame/Character.cs
@@ -0,0 +1,8 @@
+public class MainPage
+	{
+	public double Hungry;
+    public double Energy;
+    public double Thirst;
+    public string? Name;
+	
+	}
--- PousLikeGame/MainPage.xaml.cs
+++ PousLikeGame/MainPage.xaml.cs
@@ -1,10 +1,19 @@
 ﻿namespace PousLikeGame;
 
 public partial class MainPage : ContentPage
+List<Character> personagem = new List<Character>();
+Character CurrentCharacter;
 {
 	public MainPage()
 	{
 		InitializeComponent();
+		personagem.Add(new Character()
+		{
+			Hungry = 50,
+			Thirst = 50,
+			Energy = 50
+		});
+	
 	}
 }
 
